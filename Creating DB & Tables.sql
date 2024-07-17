d202f1df9b8057df3aa85b536632d115a8c9281b
KlimentC <75023728+KlimentC@users.noreply.github.com>
2024-07-17 17:15:05 +0300

    Creating DB & Tables

---
+++ Creating DB & Tables.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE pet_shop;
+USE pet_shop;
+SELECT database();
+
+CREATE TABLE cats 
+(
+name varchar(100),
+age int
+
+);
+
+
+CREATE TABLE dogs 
+(
+name varchar(50),
+breed varchar(50),
+age int
+
+);
+
+Select * from dogs;
+
+show tables;
+
+show columns from cats;
+
+desc cats;
+
+drop table cats;
+
+
+CREATE DATABASE shop;
+
+use shop;
+
+create table pasteries
+(
+name varchar(50),
+quantity int
+);
+
+desc pasteries;
+show tables;
+
+-- DROP DATABASE pet_shop;
+
+
